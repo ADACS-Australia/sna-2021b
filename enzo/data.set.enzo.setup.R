39c0a505aed014359a9f49e778c5e42ea7c607ac
David Liptai <dliptai@swin.edu.au>
2021-09-23 14:57:07 +1000

    split enzo run into two scripts

--- enzo/data.set.enzo.setup.R
+++ enzo/data.set.enzo.setup.R
@@ -179,3 +179,14 @@ create.enzo <- function(nbrNodes = NULL, nmain = NULL, nwarm = NULL, seed = 123)
 
   save(list = ls(), file = "Enzo.RData")
 }
+
+## == script: using.enzo.r
+### === the function create.enzo creates data and an initial estimation
+create.enzo(nbrNodes = 1, nmain = 20, nwarm = 20, seed = 123)
+### === arguments
+# 1. set nbrNodes to the minimum of your number of cores and 6
+# 2. set nmain to something in the range 20 to 100
+# 3. set nwarm to something in the range 20 to 200
+# 4. use your choice of random seed - this random seed will be passed to subsequent estimations
+
+### NB: you only have to create data once
--- enzo/using.enzo.R
+++ enzo/using.enzo.R
@@ -1,16 +1,5 @@
-## == script: using.enzo.r
-### === the function create.enzo creates data and an initial estimation
-create.enzo(nbrNodes = 1, nmain = 20, nwarm = 20, seed = 123)
-### === arguments
-# 1. set nbrNodes to the minimum of your number of cores and 6
-# 2. set nmain to something in the range 20 to 100
-# 3. set nwarm to something in the range 20 to 200
-# 4. use your choice of random seed - this random seed will be passed to subsequent estimations
-
-### NB: you only have to create data once
-
-
 ### === runing estimation
+library(RSienaTest)
 # load data and initial estimation
 load("Enzo.RData")
 # run sienaBayes for timing and end-to-end purposes
