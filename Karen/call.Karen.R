a9a45748e3902e3b5cdd0a09930c68aedf4490cd
Conrad Chan <conradchan@hotmail.com.au>
2021-10-26 11:30:27 +1100

    karen scripts
    amend previous commit

---
+++ Karen/call.Karen.R
@@ -0,0 +1,26 @@
+## ==== how to call Karen
+### === the function create.enzo creates data and an initial estimation
+data.set.karen.set.up(n=10,M=5,seed=123,nbrNodes=1,nmain=20,nwarm=20)
+### === arguments
+# 1. set nbrNodes to the minimum of your number of cores and 6
+# 2. set nmain to something in the range 20 to 100
+# 3. set nwarm to something in the range 20 to 200
+# 4. use your choice of random seed - this random seed will be passed to subsequent estimations
+# 5. n is the size of the network (this probably need to be greater than 10)
+# 6. M is the number of groups/networks
+
+### NB: you only have to create data once
+
+
+### === runing estimation
+# load data and initial estimation
+load('Karen.RData')
+# run sienaBayes for timing and end-to-end purposes
+# set nmain to suit your timing needs but probably not less than 10
+groupModel.ec <- sienaBayes(GroupsModel, data = my.Karen,
+                            effects = GroupEffects, priorMu = Mu, priorSigma = Sig,
+                            priorKappa = 0.01,
+                            prevBayes = groupModel.e,
+                            nmain=20, nrunMHBatches=40,
+                            nbrNodes=1, silentstart=FALSE)
+
