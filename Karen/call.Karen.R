9c017d50c899c73e851cbbba125b727f243b1d65
Conrad Chan <conradchan@hotmail.com.au>
2021-10-26 13:41:38 +1100

    separate setup script

--- Karen/call.Karen.R
+++ Karen/call.Karen.R
@@ -1,18 +1,5 @@
-## ==== how to call Karen
-### === the function create.enzo creates data and an initial estimation
-data.set.karen.set.up(n=10,M=5,seed=123,nbrNodes=1,nmain=20,nwarm=20)
-### === arguments
-# 1. set nbrNodes to the minimum of your number of cores and 6
-# 2. set nmain to something in the range 20 to 100
-# 3. set nwarm to something in the range 20 to 200
-# 4. use your choice of random seed - this random seed will be passed to subsequent estimations
-# 5. n is the size of the network (this probably need to be greater than 10)
-# 6. M is the number of groups/networks
-
-### NB: you only have to create data once
-
-
 ### === runing estimation
+library(RSienaTest)
 # load data and initial estimation
 load('Karen.RData')
 # run sienaBayes for timing and end-to-end purposes
