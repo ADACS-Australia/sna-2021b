de5d648aafceccda22bbd1608be9f0ab17ce625e
Conrad Chan <conradchan@hotmail.com.au>
2021-12-07 10:48:15 +1100

    revert debugging

--- Karen/nonblocking.r
+++ Karen/nonblocking.r
@@ -2,22 +2,3 @@ library(snow)
 
 sendData.MPInode <- function(node, data)
     Rmpi::mpi.isend.Robj(data, node$rank, node$SENDTAG, node$comm)
-
-staticClusterApply <- function(cl, fun, n, argfun) {
-    checkCluster(cl)
-    p <- length(cl)
-    if (n > 0 && p > 0) {
-        val <- vector("list", n)
-        start <- 1
-        while (start <= n) {
-            end <- min(n, start + p - 1)
-            jobs <- end - start + 1
-            for (i in 1:jobs)
-                sendCall(cl[[i]], fun, argfun(start + i - 1))
-                val[i] <- lapply(cl[i], recvResult)
-
-            start <- start + jobs
-        }
-        checkForRemoteErrors(val)
-    }
-}
