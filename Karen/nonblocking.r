1710db905b40a2216f8a9db195f726511a45d4cd
Conrad Chan <conradchan@hotmail.com.au>
2021-12-07 10:22:38 +1100

    testing: make tasks sequential

--- Karen/nonblocking.r
+++ Karen/nonblocking.r
@@ -1,2 +1,21 @@
 sendData.MPInode <- function(node, data)
     Rmpi::mpi.isend.Robj(data, node$rank, node$SENDTAG, node$comm)
+
+staticClusterApply <- function(cl, fun, n, argfun) {
+    checkCluster(cl)
+    p <- length(cl)
+    if (n > 0 && p > 0) {
+        val <- vector("list", n)
+        start <- 1
+        while (start <= n) {
+            end <- min(n, start + p - 1)
+	    jobs <- end - start + 1
+            for (i in 1:jobs)
+                sendCall(cl[[i]], fun, argfun(start + i - 1))
+                val[i] <- recvResult(cl[1:jobs])
+            # val[start:end] <- lapply(cl[1:jobs], recvResult)
+            start <- start + jobs
+        }
+        checkForRemoteErrors(val)
+    }
+}
