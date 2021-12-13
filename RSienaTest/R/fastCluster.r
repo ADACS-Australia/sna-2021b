68b545af96ef713630df48cb4b17cfb0ef1c8d35
David Liptai <dliptai@swin.edu.au>
2021-12-13 17:14:18 +1100

    indentation

--- RSienaTest/R/fastCluster.r
+++ RSienaTest/R/fastCluster.r
@@ -16,30 +16,30 @@ clusterEvalQ.SplitByRow <- function(cl, expr, xgrid) {
 }
 
 clusterExport.mpi.fast <- local({
-    env <- as.environment(1) ## .GlobalEnv
-    gets <- function(n, v) { assign(n, v, envir = env); NULL }
-    function(cl, list, envir = .GlobalEnv) {
-        ## do this with only one clusterCall--loop on workers?
-        for (name in list) {
-            clusterCall.mpi.fast(cl, gets, name, get(name, envir = envir))
-        }
-    }
+  env <- as.environment(1) ## .GlobalEnv
+  gets <- function(n, v) { assign(n, v, envir = env); NULL }
+  function(cl, list, envir = .GlobalEnv) {
+      ## do this with only one clusterCall--loop on workers?
+      for (name in list) {
+          clusterCall.mpi.fast(cl, gets, name, get(name, envir = envir))
+      }
+  }
 })
 
 clusterCall.mpi.fast <- function(cl, fun, ...) {
-    checkCluster(cl)
+  checkCluster(cl)
 
-    # create packet and serialise
-    value = list(fun = fun, args = list(...), return = TRUE, tag = NULL)
-    data_ = list(type = "EXEC", data = value, tag = NULL)
-    data = serialize(data_, NULL)
+  # create packet and serialise
+  value = list(fun = fun, args = list(...), return = TRUE, tag = NULL)
+  data_ = list(type = "EXEC", data = value, tag = NULL)
+  data = serialize(data_, NULL)
 
-    # send packet to all workers
-    for (i in seq(along = cl)) {
-        node = cl[[i]]
-        Rmpi::mpi.isend(x=data, type=4, dest=node$rank, tag=node$SENDTAG, comm=node$comm)
-    }
+  # send packet to all workers
+  for (i in seq(along = cl)) {
+      node = cl[[i]]
+      Rmpi::mpi.isend(x=data, type=4, dest=node$rank, tag=node$SENDTAG, comm=node$comm)
+  }
 
-    # recieve results from workers and return
-    checkForRemoteErrors(lapply(cl, recvResult))
+  # recieve results from workers and return
+  checkForRemoteErrors(lapply(cl, recvResult))
 }
