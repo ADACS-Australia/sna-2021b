1f4b99c29fdf611442883236738c494d5196cf2e
David Liptai <dliptai@swin.edu.au>
2021-12-13 16:31:04 +1100

    add functions to fastCluster.r

--- Karen/test_mpi.R
+++ Karen/test_mpi.R
@@ -6,40 +6,6 @@ args = parser$parse_args()
 # Load setup
 load(args$file)
 
-# Add source mods here
-# e.g.
-# insertSource("../snow_mod.r")
-
-clusterExport.mpi.fast <- local({
-    env <- as.environment(1) ## .GlobalEnv
-    gets <- function(n, v) { assign(n, v, envir = env); NULL }
-    function(cl, list, envir = .GlobalEnv) {
-        ## do this with only one clusterCall--loop on workers?
-        for (name in list) {
-            clusterCall.mpi.fast(cl, gets, name, get(name, envir = envir))
-        }
-    }
-})
-
-clusterCall.mpi.fast <- function(cl, fun, ...) {
-    checkCluster(cl)
-
-    # create packet and serialise
-    value = list(fun = fun, args = list(...), return = TRUE, tag = NULL)
-    data_ = list(type = "EXEC", data = value, tag = NULL)
-    data = serialize(data_, NULL)
-
-    # send packet to all workers
-    for (i in seq(along = cl)) {
-        node = cl[[i]]
-        Rmpi::mpi.isend(x=data, type=4, dest=node$rank, tag=node$SENDTAG, comm=node$comm)
-    }
-
-    # recieve results from workers and return
-    checkForRemoteErrors(lapply(cl, recvResult))
-}
-
-
 # Run sienaBayes
 groupModel.ec = RSienaTest::sienaBayes(GroupsModel,
   data = my.Karen,
